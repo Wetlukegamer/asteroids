8ff161994d50d36b417615105b0a4ebe4e2bc007
Wetlukegamer <lukearcher2012@outlook.com>
2026-06-28 22:23:28 -0500

    add groups to main.py for organization

--- main.py
+++ main.py
@@ -12,6 +12,12 @@ def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f'''Screen width: {SCREEN_WIDTH}
 Screen height: {SCREEN_HEIGHT}''')
+    # create groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    # manage group-object relations
+    Player.containers = (updatable, drawable)
+    # create objects
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     # Loop constantly
@@ -21,10 +27,11 @@ Screen height: {SCREEN_HEIGHT}''')
             if event.type == pygame.QUIT:
                 return
         # imput check
-        player.update(dt)
+        updatable.update(dt)
         # Draw sprites
         screen.fill("black")
-        player.draw(screen)
+        for sprite in drawable:
+            sprite.draw(screen)
         # Refresh
         pygame.display.flip()
         dt = clock.tick(60) / 1000
