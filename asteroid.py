c5b14a9760c996fadc418a99521c1c10109c0b42
Wetlukegamer <lukearcher2012@outlook.com>
2026-06-29 20:28:33 -0500

    add asteroid spliting

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,7 @@
-import pygame
+import pygame, random
 from circleshape import CircleShape
-from constants import LINE_WIDTH
+from constants import LINE_WIDTH, ASTEROID_MIN_RADIUS, ASTEROID_MAX_RADIUS
+from logger import log_event
 
 class Asteroid(CircleShape):
     def __init__(self, x: float, y: float, radius: float) -> None:
@@ -11,4 +12,18 @@ class Asteroid(CircleShape):
 
     def update(self, dt: float) -> None:
         self.position += self.velocity * dt
-        
+
+    def split(self) -> None:
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS: return
+        log_event("asteroid_split")
+        random_angle = random.uniform(20, 50)
+        init_velocity = self.velocity * 1.2
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        # spawn asteroids
+        def new_asteroid() -> "Asteroid":
+            return Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid_1, asteroid_2 = new_asteroid(), new_asteroid()
+        # apply velocity
+        asteroid_1.velocity = init_velocity.rotate(random_angle)
+        asteroid_2.velocity = init_velocity.rotate(-random_angle)
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import pygame
-import sys
+import pygame, sys
 from constants import *
 from logger import log_state, log_event
 from player import Player
@@ -54,7 +53,7 @@ Screen height: {SCREEN_HEIGHT}''')
                 if asteroid.colides_with(shot):
                     log_event("asteroid_shot")
                     shot.kill()
-                    asteroid.kill()
+                    asteroid.split()
         dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
